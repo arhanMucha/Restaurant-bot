0de5d3ed8495e70304882e478c53d146dc3550ea
arhanMucha <arhanmuchakani@gmail.com>
2026-04-05 01:22:31 +0200

    Delete restaurant-bot/api directory

--- restaurant-bot/api/config.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  res.json({
-    businessName: process.env.BUSINESS_NAME || 'Our Restaurant',
-    calendarConnected: !!process.env.GOOGLE_REFRESH_TOKEN
-  });
-}
